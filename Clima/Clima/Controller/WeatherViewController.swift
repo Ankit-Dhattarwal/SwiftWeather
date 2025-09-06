fe97e0deaf3d338853859ec5a5b9a46ac2215dae
Ankit Dhattarwal <109728587+Ankit-Dhattarwal@users.noreply.github.com>
2025-09-06 16:14:44 +0530

    Add files via upload
    "Initial comment with Note
    -- Note for  how to change the background for dark and light mode 
    -- Note for the how to change the color of any icon , lable and more for the dark and light mode.
    "

---
+++ Clima/Clima/Controller/WeatherViewController.swift
@@ -0,0 +1,38 @@
+
+
+/*
+ 
+ NOTE:
+ 
+ >>>>>>>>>  How to make dynamic color according to System.
+ 
+ When we define the custom color then not able to change with the system color mode like dark , light.
+ 
+ So when pickup the color using drop-pin then right click on chose color box and add on the small colors box so that when we add color set in the assets then we able to get that color and in this we define color for light and dark mode.
+ 
+ And at last on original main file we choose the custom color but we use that define color that show on the top.
+ 
+ 
+ >>> Sometime we use png photo for the background and sometime pdf
+ 
+ In this when we use the png then issue come as on zoom photo clearlty gone.
+ But we use the pdf for background image this not lossup their zooming
+ But when use the pdf as image then make sure enable (click the checkbox)the **Resizing**.
+ */
+
+import UIKit
+
+class WeatherViewController: UIViewController {
+
+    @IBOutlet weak var conditionImageView: UIImageView!
+    @IBOutlet weak var temperatureLabel: UILabel!
+    @IBOutlet weak var cityLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
